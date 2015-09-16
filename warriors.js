95ab3564a3d3421470ed00f5a4302c2642272d79
Marie-Eve Gauthier <gauthier.marie.eve@gmail.com>
2015-09-16 21:06:44 +0000

    created a warriors object and some variables using it

--- warriors.js
+++ warriors.js
@@ -1,1 +1,37 @@
-//Hello!
+//Let's go! The warriors are coming!
+
+function Warriors(name, gender) {
+    this.name = name;
+    this.gender = gender;
+    this.level = 1;
+    this.weapon = "wooden sword";
+    this.power = Math.floor(Math.random() * (100)) + 1;
+};
+    
+Warriors.prototype = {
+    fight: function(){
+        console.log(this.name +" rushes to the arena with " + this.weapon);
+    },
+    faceoff: function(opponent){
+        if(this.power > opponent.power){
+            console.log("Hourra!" + this.name + ", you're so powerful! You won");
+        }
+        else if(this.power < opponent.power){
+            console.log("Hourra!" + opponent.name + ", you're so powerful! You won");
+        }
+        else if (this.power === opponent.power){
+            console.log("Unbelievable! After hours of combat, you're still alive both of you.") 
+        }
+    }
+};
+
+
+var robin = new Warriors("Robin", "M");
+var arthur = new Warriors("Arthur", "M");
+var jeanne = new Warriors("Jeanne", "F");
+
+robin.fight();
+arthur.fight();
+robin.faceoff(arthur);
+arthur.faceoff(jeanne);
+    
