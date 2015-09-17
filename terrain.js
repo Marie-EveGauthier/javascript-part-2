4a5dd8bd056ebdf7477ccafd7ac3aec753a35c69
Marie-Eve Gauthier <gauthier.marie.eve@gmail.com>
2015-09-17 03:29:37 +0000

    created all the properties of Tile.prototype

--- terrain.js
+++ terrain.js
@@ -1,1 +1,38 @@
 // Hello!
+function Tile(x,y){
+    this.x = x;
+    this.y = y;
+    this.height = Math.floor(Math.random()*(3)+ 1); 
+    var typesOfTile = ['grass', 'rocks', 'water'];
+    this.type = typesOfTile[Math.floor(Math.random() * typesOfTile.length)];
+
+    var conditionsOfTile = ['burning', 'normal', 'frozen'];
+    this.condition = conditionsOfTile[Math.floor(Math.random() * conditionsOfTile.length)]; 
+    
+};
+
+Tile.prototype = {
+    freeze: function (){
+        if (this.condition === "burning") {
+            this.condition === "normal";
+        }
+        else if (this.condition === "normal"){
+            this.condition === "frozen";
+        }
+        else if (this.condition === "frozen") {
+            this.height += 1;
+        }
+    },
+    burn: function(){
+        if (this.condition === "frozen") {
+            this.condition === "normal";
+        }
+        else if (this.condition === "normal") {
+             this.condition === "burning";
+        }
+        else if (this.condition === "burning") {
+            this.heigth -= 1; 
+            this.heigth >= 0;
+        }
+    }
+};
