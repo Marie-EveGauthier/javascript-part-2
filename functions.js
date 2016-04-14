bacdd737b9e0a79046cff1ede5b339cba23594b4
Marie-Eve Gauthier <gauthier.marie.eve@gmail.com>
2016-04-14 20:31:56 +0000

    array of contacts emails sorted by the longest name or by email address in alphabetical order

--- functions.js
+++ functions.js
@@ -18,3 +18,41 @@ function  longestString (str1, str2) {
 var animals = ["butterfly", "horse", "bird", "cat", "rabbit"];
  
 console.log(animals.sort(longestString));
+
+/*Create an array of objects (don’t need to use new here, just regular object literals). 
+These objects will contain a name and email property. Then, run some code that will sort your array by the longest name. 
+Then, run some code that will sort your array by e-mail address in alphabetical order.
+*/
+
+var myContactsEmail = [
+    {name:"Annie-Claude", email:"annie_claude_beaudry@gmail.com"},
+    {name:"Corinne", email:"coco.lalande@hotmail.com"},
+    {name:"Paco", email:"franciscovia@yahoo.es"},
+    ];
+    
+//array sorted by the longest name  
+console.log(myContactsEmail.sort(function (a, b) {
+  if (a.name.length > b.name.length) {
+    return 1;
+  }
+  if (a.name.length < b.name.length) {
+    return -1;
+  }
+  // a must be equal to b
+  return 0;
+}));
+
+//array sorted by e-mail address in alphabetical order
+console.log(myContactsEmail.sort(function(a, b) {
+if (a.email > b.email) {
+    return 1;
+  }
+  if (a.email < b.email) {
+    return -1;
+  }
+  // a must be equal to b
+  return 0;    
+    
+}));
+
+    
